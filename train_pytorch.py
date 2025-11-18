e7ff7e2551e2f554b5d0b62828eb5584f91e016b
Martin Ng <martinng2002@gmail.com>
2025-11-18 13:29:34 +0800

    ClearML: model_training in Full Overview

--- train_pytorch.py
+++ train_pytorch.py
@@ -10,7 +10,8 @@ from torchvision import datasets, transforms
 
 task = Task.init(
     project_name="Full Overview",
-    task_name="model_training"
+    task_name="model_training",
+    reuse_last_task_id=True
 )
 
 # Connect hyperparameters as args
